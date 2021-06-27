b0dd947f172bda3b717c36bb428dce5d88f5efe8
chuynh17 <79731109+chuynh17@users.noreply.github.com>
2021-06-27 10:09:05 -0500

    Add files via upload

---
+++ Challenge/MechaCarChallenge.R
@@ -0,0 +1,49 @@
+demo_table2 <- fromJSON(txt='demo.json')
+demo_table[3, "Year"]
+
+# Deliverable 1
+
+#load dplyr package
+library(dplyr)
+
+#read file
+MechaCar <- read.csv(file='MechaCar_mpg.csv',check.names=F,stringsAsFactors = F)
+
+#perform linear regression
+lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data= MechaCar)
+
+#perform summary
+summary(lm(mpg ~ vehicle_length + vehicle_weight + spoiler_angle + ground_clearance + AWD,data= MechaCar))
+
+
+
+# Deliverable 2
+
+#read file
+mecha_coil <- read.csv(file='Suspension_Coil.csv',check.names=F,stringsAsFactors = F) 
+
+#get a total summary
+total_summary <- mecha_coil %>% summarize(Mean_PSI=mean(PSI),
+                                          Median_PSI=median(PSI),
+                                          Var_PSI=var(PSI),
+                                          Std_Dev_PSI=sd(PSI),
+                                          Num_Coil=n(), .groups = 'keep') 
+
+#create a lot summary
+lot_summary <- mecha_coil  %>% group_by(Manufacturing_Lot) %>% summarize(Mean_PSI=mean(PSI),
+                                                                         Median_PSI=median(PSI),
+                                                                         Var_PSI=var(PSI),
+                                                                         Std_Dev_PSI=sd(PSI),
+                                                                         Num_Coil=n(), .groups = 'keep')   
+
+
+# Deliverable 3
+#perform t-test to determine if the PSI across 
+# Peform t-test across all Lots
+t.test(Suspension$PSI,mu = 1500)
+# Peform t-test on Lot 1
+t.test(subset(Suspension,Manufacturing_Lot=="Lot1")$PSI,mu = 1500)
+# Peform t-test on Lot 2
+t.test(subset(Suspension,Manufacturing_Lot=="Lot2")$PSI,mu = 1500)
+# Peform t-test on Lot 3
+t.test(subset(Suspension,Manufacturing_Lot=="Lot3")$PSI,mu = 1500)
